dea5405a7338b0f34280e42eb074e65b2de7588e
Narongsak kongpan <disengage.nrskp@gmail.com>
2022-07-15 02:00:46 +0700

    Print result

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 //var whatIsIt string
 //secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
 //sd, _ := base64.StdEncoding.DecodeString(secret)
+
+const secret = "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+const decoded = window.atob(secret)
+const sd = decoded.split("").reverse().join("")
+console.log(sd)
